ed823af41e70ff9a378669fa3ff16082c2a4756d
khalidhub7 <louguikhalid7@gmail.com>
2025-07-17 01:38:41 +0100

    use class instead of normal routes

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,25 +1,23 @@
-import express from 'express';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-const router = express.Router();
+class AppController {
+  static async getStatus(_, res) {
+    const isRedisAlive = redisClient.isAlive();
+    const isDbAlive = dbClient.isAlive();
 
-router.get('/status', (_, res) => {
-  const isRedisAlive = redisClient.isAlive();
-  const isDbAlive = dbClient.isAlive();
+    // .send() auto adds Content-Type: application/json
+    const statusCode = (isRedisAlive && isDbAlive)
+      ? 200 : 500;
+    res.status(statusCode)
+      .send({ redis: isRedisAlive, db: isDbAlive });
+  }
 
-  // .send() auto adds Content-Type: application/json
-  const statusCode = (isRedisAlive && isDbAlive)
-    ? 200 : 500;
-  res.status(statusCode)
-    .send({ redis: isRedisAlive, db: isDbAlive });
-});
+  static async getStats(_, res) {
+    const files = await dbClient.nbFiles();
+    const users = await dbClient.nbUsers();
 
-router.get('/stats', async (_, res) => {
-  const files = await dbClient.nbFiles();
-  const users = await dbClient.nbUsers();
-
-  res.status(200).send({ users, files });
-});
-
-export default router;
+    res.status(200).send({ users, files });
+  }
+}
+export default AppController;
