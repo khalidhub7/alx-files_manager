99d5e548f65d8cc8a72a89a0889655286a274ee6
khalidhub7 <louguikhalid7@gmail.com>
2025-07-18 03:52:19 +0100

    authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,11 @@ import AuthController from '../controllers/AuthController';
 const router = express.Router();
 
 // load AppController routes at '/'
-router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/status', AppController.getStatus);
 router.post('/users', UsersController.postNew);
-
+router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
 
 export default router;
