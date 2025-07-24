a5030ac759f84ed0c4b811ef41cd3a5f76377eaa
khalidhub7 <louguikhalid7@gmail.com>
2025-07-24 06:45:04 +0100

    setup

--- scripts/api_examples.sh
+++ scripts/api_examples.sh
@@ -14,13 +14,13 @@ curl http://0.0.0.0:5000/connect \
 curl http://0.0.0.0:5000/users/me \
   -H "x-token: a5422410-e28e-4ed1-9799-6de1bcc05db6" && echo ""
 
-# logout using x-token
+# logout user with x-token
 curl http://0.0.0.0:5000/disconnect \
   -H "x-token: a5422410-e28e-4ed1-9799-6de1bcc05db6" && echo ""
 
 # files
 
-# upload a file (base64 encoded content)
+# upload a file (base64 encoded)
 curl -X POST http://0.0.0.0:5000/files \
   -H "x-token: a5422410-e28e-4ed1-9799-6de1bcc05db6" \
   -H "content-type: application/json" \
@@ -32,20 +32,26 @@ curl -X POST http://0.0.0.0:5000/files \
   -H "content-type: application/json" \
   -d '{ "name": "images", "type": "folder" }' && echo ""
 
-# get file details by file id (must belong to the user)
-curl -X GET 0.0.0.0:5000/files/5f1e8896c7ba06511e683b25 \
+# get file details by file id (must belong to user)
+curl -X GET http://0.0.0.0:5000/files/5f1e8896c7ba06511e683b25 \
   -H "x-token: f21fb953-16f9-46ed-8d9c-84c6450ec80f" && echo ""
 
-# get first 20 files (default page = 0)
-curl -X GET 0.0.0.0:5000/files \
+# get first 20 files (page 0 by default)
+curl -X GET http://0.0.0.0:5000/files \
   -H "x-token: f21fb953-16f9-46ed-8d9c-84c6450ec80f" && echo ""
 
-# get files under specific folder (parentId), first 20 results (default page = 0)
-curl -X GET "0.0.0.0:5000/files?parentId=5f1e881cc7ba06511e683b23" \
+# get files in folder by parentId (first 20 results, page 0)
+curl -X GET "http://0.0.0.0:5000/files?parentId=5f1e881cc7ba06511e683b23" \
   -H "x-token: f21fb953-16f9-46ed-8d9c-84c6450ec80f" && echo ""
 
+# set file public by id and x-token
+curl -X PUT http://0.0.0.0:5000/files/5f1e8896c7ba06511e683b25/publish \
+  -H "x-token: f21fb953-16f9-46ed-8d9c-84c6450ec80f" && echo ""
 
+# set file not public by id and x-token
+curl -X PUT http://0.0.0.0:5000/files/5f1e8896c7ba06511e683b25/unpublish \
+  -H "x-token: f21fb953-16f9-46ed-8d9c-84c6450ec80f" && echo ""
 
-
-# current x-token
-a5422410-e28e-4ed1-9799-6de1bcc05db6
+# get file content by file id and x-token
+curl -X GET http://0.0.0.0:5000/files/5f1e879ec7ba06511e683b22/data \
+  -H "x-token: f21fb953-16f9-46ed-8d9c-84c6450ec80f" && echo ""
