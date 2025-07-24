2cea2390d220652727b92201f44df63da62e946b
khalidhub7 <louguikhalid7@gmail.com>
2025-07-24 05:00:46 +0100

    files handling

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -42,7 +42,7 @@ class FilesController {
 
     // shared metadata for both files and folders
     let newFileOrFolder = {
-      userId: userID, name, type, isPublic, parentId,
+      userId: userID, name, type, isPublic, parentId: Number(parentId),
     };
 
     // handle folder creation
