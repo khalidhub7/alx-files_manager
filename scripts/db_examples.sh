4bca21ec0920b551ee966130ba3ad070e2cb75b6
khalidhub7 <louguikhalid7@gmail.com>
2025-07-24 06:48:23 +0100

    setup

--- scripts/db_examples.sh
+++ scripts/db_examples.sh
@@ -1,30 +1,44 @@
-# for file
-# saved to db:
+# File example
+
+# Stored in DB (includes local file path)
 {
   "_id": ObjectId("6881b351c2371e129814450e"),
   "userId": ObjectId("6881863ea1d8275a6a3b58c4"),
-  "name": "myText.txt", "type": "file", "isPublic": false, "parentId": 0,
+  "name": "myText.txt",
+  "type": "file",
+  "isPublic": false,
+  "parentId": 0,
   "localPath": "/tmp/files_manager/a499e837-...c8401"
 }
-# api response:
+
+# API response (no localPath, id as string)
 {
   "id": "6881b351c2371e129814450e",
   "userId": "6881863ea1d8275a6a3b58c4",
-  "name": "myText.txt", "type": "file", "isPublic": false, "parentId": 0
+  "name": "myText.txt",
+  "type": "file",
+  "isPublic": false,
+  "parentId": 0
 }
 
+# Folder example
 
-# for folder
-# saved to db:
+# Stored in DB
 {
   "_id": ObjectId("6881b358c2371e129814450f"),
   "userId": ObjectId("6881863ea1d8275a6a3b58c4"),
-  "name": "images", "type": "folder", "isPublic": false, "parentId": 0
+  "name": "images",
+  "type": "folder",
+  "isPublic": false,
+  "parentId": 0
 }
 
-# api response:
+# API response (id as string)
 {
   "id": "6881b358c2371e129814450f",
   "userId": "6881863ea1d8275a6a3b58c4",
-  "name": "images", "type": "folder", "isPublic": false, "parentId": 0
+  "name": "images",
+  "type": "folder",
+  "isPublic": false,
+  "parentId": 0
 }
