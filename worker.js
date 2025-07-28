8a3f4363f14673f1c98f07e063f7afee1798fa50
khalidhub7 <louguikhalid7@gmail.com>
2025-07-28 20:15:02 +0100

    improve worker

--- worker.js
+++ worker.js
@@ -6,35 +6,34 @@ import UtilsHelper from './utils/utils';
 const fileQueue = new Bull('image_resizer');
 
 fileQueue.process(async (job) => {
-  try {
-    const { fileId, userId } = job.data;
+  const { fileId, userId } = job.data;
 
-    if (!fileId) { throw new Error('Missing fileId'); }
-    if (!userId) { throw new Error('Missing userId'); }
+  if (!fileId) throw new Error('Missing fileId');
+  if (!userId) throw new Error('Missing userId');
 
-    const fileDoc = await UtilsHelper
-      .getFileByIdAndUser(fileId, userId);
+  const fileDoc = await UtilsHelper
+    .getFileByIdAndUser(fileId, userId);
+  if (!fileDoc) throw new Error('File not found');
 
-    if (!fileDoc) { throw new Error('File not found'); }
+  const supportedWidths = [100, 250, 500];
 
-    const supportedWidths = [100, 250, 500];
+  await Promise.all(supportedWidths.map(async (width) => {
+    const resizedBuffer = await thumbnail(
+      fileDoc.localPath, { width },
+    );
+    await fsPromises.writeFile(
+      `${fileDoc.localPath}_${width}`, resizedBuffer,
+    );
+    console.log(`_> thumbnail ${width} generated`);
+  }));
+});
 
-    await Promise.all(supportedWidths.map(async (width) => {
-      try {
-        const resizedBuffer = await thumbnail(
-          fileDoc.localPath, { width },
-        );
-        await fsPromises.writeFile(
-          `${fileDoc.localPath}_${width}`, resizedBuffer,
-        );
-        console.log(`thumbnail ${width} generated`);
-      } catch (err) {
-        console.log(`thumbnail ${width} failed: ${err.message}`);
-      }
-    }));
-  } catch (err) {
-    console.log(`job failed: ${err.message}`);
-  }
+fileQueue.on('completed', (job) => {
+  console.log(`_> Job ${job.id} completed`);
+});
+
+fileQueue.on('failed', (job, err) => {
+  console.log(`_> Job ${job.id} failed: ${err.message}`);
 });
 
 export default fileQueue;
