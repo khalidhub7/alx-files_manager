6618dd00176f4ab35d6ba13ed01c86fbc8017a32
khalidhub7 <louguikhalid7@gmail.com>
2025-07-29 05:01:27 +0100

    upload

---
+++ tests/image_upload.py
@@ -0,0 +1,31 @@
+import base64
+import requests
+import sys
+import json
+
+file_path = sys.argv[1]
+file_name = file_path.split('/')[-1]
+
+file_encoded = None
+with open(file_path, "rb") as image_file:
+    file_encoded = base64.b64encode(image_file.read()
+                                    ).decode('utf-8')
+
+r_json = {
+    'name': file_name, 'type': 'image',
+    'isPublic': True, 'data': file_encoded,
+    'parentId': sys.argv[3]
+}
+
+r_headers = {'X-Token': sys.argv[2]}
+
+r = requests.post("http://0.0.0.0:5000/files",
+                  json=r_json, headers=r_headers)
+# print(r.json())
+
+print(json.dumps({
+    "status": r.status_code,
+    "body": r.json()
+}))
+
+# python script.py <file_path> <X-Token> <parentId>
